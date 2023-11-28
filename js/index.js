6f4db9da7315a35dbb99040e2df4e3ae370e59ba
chunmujin <anwls0276@naver.com>
2023-11-28 10:55:38 +0900

    홈페이지 기초

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetElement = document.querySelector(this.getAttribute('href'));
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
